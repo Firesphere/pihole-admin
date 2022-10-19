aa8f601b933256b90c6b125f6e373cfd79597407
Simon Erkelens <github@casa-laguna.net>
2022-10-19 23:11:49 +1300

    Add CPU, Memory and Temperature readings

---
+++ app/SysInfo.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App;
+
+class SysInfo
+{
+    public static function getAll()
+    {
+        return [
+            self::getMemoryUse(),
+            self::getLoad(),
+            self::getCPUCount(),
+            self::getTemperature()
+        ];
+    }
+
+    public static function getMemoryUse()
+    {
+        $file = fopen('/proc/meminfo', 'r');
+        $meminfo = [];
+        while ($line = fgets($file)) {
+            $expl = explode(':', $line);
+            if (count($expl) == 2) {
+                $kb = rtrim(trim($expl[1]), ' kB');
+                $meminfo[rtrim($expl[0], ':')] = $kb;
+            }
+        }
+        $memused = $meminfo['MemTotal'] - $meminfo['MemFree'] - $meminfo['Buffers'] - $meminfo['Cached'];
+        return $memused / $meminfo['MemTotal'];
+    }
+
+    public static function getLoad()
+    {
+        return sys_getloadavg();
+    }
+
+    public static function getCPUCount()
+    {
+        $cpuinfo = file_get_contents('/proc/cpuinfo');
+        preg_match_all('/^processor/m', $cpuinfo, $matches);
+        return count($matches[0]);
+    }
+
+    public static function getTemperature()
+    {
+        if (file_exists('/sys/class/thermal/thermal_zone0/temp')) {
+            $output = rtrim(file_get_contents('/sys/class/thermal/thermal_zone0/temp'));
+        } elseif (file_exists('/sys/class/hwmon/hwmon0/temp1_input')) {
+            $output = rtrim(file_get_contents('/sys/class/hwmon/hwmon0/temp1_input'));
+        } else {
+            $output = '';
+        }
+
+        // Test if we succeeded in getting the temperature
+        if (is_numeric($output)) {
+            // $output could be either 4-5 digits or 2-3, and we only divide by 1000 if it's 4-5
+            // ex. 39007 vs 39
+            $celsius = (int)$output;
+
+            // If celsius is greater than 1 degree and is in the 4-5 digit format
+            if ($celsius > 1000) {
+                // Use multiplication to get around the division-by-zero error
+                $celsius *= 1e-3;
+            }
+
+        } else {
+            // Nothing can be colder than -273.15 degree Celsius (= 0 Kelvin)
+            // This is the minimum temperature possible (AKA absolute zero)
+            $celsius = false;
+        }
+
+        return $celsius;
+    }
+}
--- config/twig.php
+++ config/twig.php
@@ -2,6 +2,7 @@
 
 // Create Twig
 use App\API\PiHole;
+use App\SysInfo;
 use Slim\Views\Twig;
 
 $versions = (new PiHole())->getParsedVersions();
@@ -11,12 +12,17 @@ foreach ($versions as $key => &$value) {
 }
 unset($value);
 $twig = Twig::create(__DIR__ . '/../templates');
-
+[$mem, $load, $cpu, $temp] = SysInfo::getAll();
 $twigEnv = $twig->getEnvironment();
 $twigEnv->addGlobal('hostname', 'Bababerry');
 $twigEnv->addGlobal('DockerVersion', $versions['DOCKER_VERSION'] ?? false);
 $twigEnv->addGlobal('CoreVersion', $versions['CORE_VERSION']);
 $twigEnv->addGlobal('FTLVersion', $versions['FTL_VERSION']);
 $twigEnv->addGlobal('WebVersion', "1.0.0");
-
+$twigEnv->addGlobal('Load0', number_format($load[0], 2));
+$twigEnv->addGlobal('Load1', number_format($load[1], 2));
+$twigEnv->addGlobal('Load2', number_format($load[2], 2));
+$twigEnv->addGlobal('MemUse', number_format($mem, 4));
+$twigEnv->addGlobal('CPUCount', $cpu);
+$twigEnv->addGlobal('Temperature', $temp);
 return $twig;
